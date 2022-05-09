fdb159a5a6e1c5f66e399f6fadb81c65b1bfc6da
TJ <TJ@DESKTOP-1POLRJ4>
2022-05-09 12:18:54 +0900

    s

--- src/sc6.java
+++ src/sc6.java
@@ -1,28 +1,38 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class sc6 {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		String id;
-		String pw;
-		
-		System.out.println("아이디 입력");
-		id = sc.next();
-		System.out.println("비밀번호 입력");
-		pw = sc.next();
-		
-		if(id.equals("admin")  && pw.equals("1111") ) {
-			System.out.println("로그인성공"); 
-		}else {
-			System.out.println("로그인실패");
-		}
-		
-		
+//		Scanner sc = new Scanner(System.in);
+//		String id;
+//		String pw;
+//		
+//		System.out.println("아이디 입력");
+//		id = sc.next();
+//		System.out.println("비밀번호 입력");
+//		pw = sc.next();
+//		
+//		if(id.equals("admin")  && pw.equals("1111") ) {
+//			System.out.println("로그인성공"); 
+//		}else {
+//			System.out.println("로그인실패");
+//		}
 		
 		
+		int value = 356;
+		System.out.println(value-56);
 		
+		int top = 5;
+		int bottom = 10;
+		int height = 7;
+		double area = (top+bottom)*height / 2.0;
+		System.out.println(area);
+		
+		int x =10;
+		int y = 5;
+		System.out.println((x>7 && (y<=5)));
+		System.out.println((x%3 == 2) ||(y%2 != 1));
 		
 	}
 
